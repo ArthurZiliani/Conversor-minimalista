399cad46af5b30ba6590b74c4c5f87cfd5ebe2bf
Rhels Ziliani <arthur.andrade01@fatec.sp.gov.br>
2020-09-10 04:01:02 -0300

    Conversor binario para decimal em javascript

---
+++ calculo.js
@@ -0,0 +1,16 @@
+function convert(){
+
+const binario = document.getElementById('valor').value;
+
+const decimal = parseInt(binario, 2);
+
+document.getElementById('resultado').value = decimal;
+
+}
+
+
+
+       
+
+
+
